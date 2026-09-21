758a4d0b7e6767cfc09277fae1b4f184348393c8
hrzanni <hrzanni@gmail.com>
2026-09-21 15:30:35 -0300

    Adicionando o projeto spring e suas depencias

---
+++ startinvest-backend/src/main/java/com/startinvest/startinvest_backend/StartinvestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.startinvest.startinvest_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartinvestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartinvestBackendApplication.class, args);
+	}
+
+}
---
+++ startinvest-backend/src/test/java/com/startinvest/startinvest_backend/StartinvestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startinvest.startinvest_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartinvestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
